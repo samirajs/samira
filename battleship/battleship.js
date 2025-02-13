dc8766bb1d9d8b98b4c723ff7cf303781f9e8a8e
samira <miralivev7645@gmail.com>
2025-02-13 22:36:32 +0600

    Add files via upload

---
+++ battleship/battleship.js
@@ -0,0 +1,30 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+//Объявление переменнных
+
+while (isSunk == false) {
+    guess = prompt("Ready,caim, fire! (eneter a number 0-6):");
+    if (guess < - 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3){
+            hits = hits + 1;
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my Battleship!");
+            }
+        }
+    }
+} 
+var stats = "You took " + guesses + "guesses to sink the battleship, " +
+                "wich means your shooting accuracy was " + (3/guesses);
+alert(stats);                
